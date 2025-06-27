3aafede502e54c963f07581e974899b14b0c9f9b
PITTAWAT <dev@dosetech.co>
2025-06-27 13:53:15 +0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("สวัสดีจาก JavaScript!");
+const btn = document.getElementById("toggle");
+const menu = document.getElementById("navbar");
+const close = document.getElementById("close");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
+
+close.addEventListener("click", () => {
+  menu.classList.remove("open");
+});
